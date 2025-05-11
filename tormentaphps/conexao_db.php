0d7899804fddcbfa0e8cc28c478c2d456d29cb2e
psykphen <thraway671@gmail.com>
2025-05-11 20:29:43 -0300

    conexao

--- tormentaphps/conexao_db.php
+++ tormentaphps/conexao_db.php
@@ -1,9 +1,9 @@
 <?php
 if (!function_exists("conexao")) {
 	function conexao() {
-		$host = '192.168.122.175';
-		$user = 'user';
-		$pass = 'qwe';
+		$host = 'localhost';
+		$user = 'root';
+		$pass = '';
 		$dbname = 'tormentasite_db';
 
 		$conn = mysqli_connect("$host", "$user", "$pass", "$dbname");
